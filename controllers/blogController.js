1b50d98aed2ac20a2e43d201abd7fb06a9a12eee
vtouzard <vtouzard244@gmail.com>
2024-03-26 00:36:33 +0100

    feat: Ajout de la méthode pour ajouter un blog

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,21 +1,49 @@
 import mongoose from "mongoose";
-const Blog = require('../models/blog')
+const Blog = require("../models/blog");
 
 // fetch list of blogs
-const fetchAllBlog = async(req,res) => {
-    let blogList;
-    try {
-        blogList = await Blog.find();
-    } catch (error) {
-        console.error(error);
-    }
+const fetchAllBlog = async (req, res) => {
+  let blogList;
+  try {
+    blogList = await Blog.find();
+  } catch (error) {
+    console.error(error);
+  }
 
-    if(!blogList){
-        return res.status(404).json({message: 'No Blogs Found'})
-    }
+  if (!blogList) {
+    return res.status(404).json({ message: "No Blogs Found" });
+  }
 
-    return res.status(200).json({blogList})
-}
+  return res.status(200).json({ blogList });
+};
 // add a new blog
+
+const addNewBlog = async (req, res) => {
+  const { title, description } = req.body;
+  const currentDate = new Date();
+
+  const newlyCreateBlog = new Blog({
+    title,
+    description,
+    date: currentDate,
+  });
+
+  try {
+    await newlyCreateBlog.save();
+  } catch (error) {
+    console.error(error);
+  }
+
+  try {
+    const session = mongoose.startSession();
+    (await session).startTransaction();
+    await newlyCreateBlog.save(session);
+    (await session).commitTransaction();
+  } catch (error) {
+    return res.send(500).json({ message: error });
+  }
+
+  return res.status(200).json({ newlyCreateBlog });
+};
 // delete a blog
-// update a blog 
+// update a blog
